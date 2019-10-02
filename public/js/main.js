24f19bc8e0a64270161da239c6c0c768fd28ca1c
keita <banira0428@gmail.com>
2019-10-02 20:27:02 +0900

    クリックイベント実装

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,26 @@
 let currentUID = "";
 const signOutButton = document.getElementById('logout');
 
+initRouter = function () {
+  this.router = new Navigo();
+
+  var that = this;
+  this.router
+    .on({
+      '/': function () {
+        loadTests();
+      }
+    })
+    .on({
+      '/tests/*': function () {
+        var path = that.getCleanPath(document.location.pathname);
+        var id = path.split('/')[2];
+        that.viewRestaurant(id);
+      }
+    })
+    .resolve();
+};
+
 window.addEventListener('load', function () {
   // Bind Sign in button.
   // if(currentUID === ""){
@@ -24,6 +44,7 @@ function onAuthStateChanged(user) {
 
   if (user) {
     currentUID = user.uid;
+    initRouter();
     loadTests();
   } else {
     // Set currentUID to null.
@@ -38,9 +59,14 @@ function loadTests() {
   let stockList = '';
   firebase.firestore().collection("tests").limit(50).where('userId', '==', currentUID).get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.data().name} => ${doc.id}`);
-      stockList += '<li class="test">' + doc.data().name + '</li>';　// = ではなく += を使う
+      stockList += '<a href="#" class="clickable" id=' + doc.id + '><li class="test">' + doc.data().name + '</li></a>';
     });
     document.getElementById('tests').innerHTML = stockList;
+    let tests = document.getElementById("tests").children;
+    for (let i = 0; i < tests.length; i++) {
+      tests[i].addEventListener('click', function () {
+        console.log(tests[i].id);
+      });
+    }
   });
 }
