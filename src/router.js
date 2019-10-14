ffbce2300c0e918c8304b1f4ca7c266218532412
keita <banira0428@gmail.com>
2019-10-14 17:14:29 +0900

    利用規約　追加

--- src/router.js
+++ src/router.js
@@ -4,38 +4,41 @@ import Tests from './components/Tests';
 import Questions from './components/Questions';
 import SignIn from './components/SignIn';
 import About from './components/About';
+import Terms from './components/Terms';
 import firebase from 'firebase';
 
 Vue.use(VueRouter);
 
 const routes = [
-    {path: '/', component: Tests},
-    {path: '/questions/:id', component: Questions},
-    {path: '/signin', component: SignIn, meta: {isPublic: true}},
-    {path: '/about', component: About, meta: {isPublic: true}}
+  {path: '/', component: Tests},
+  {path: '/questions/:id', component: Questions},
+  {path: '/signin', component: SignIn, meta: {isPublic: true}},
+  {path: '/about', component: About, meta: {isPublic: true}},
+  {path: '/terms', component: Terms, meta: {isPublic: true}}
+
 ];
 
 const router = new VueRouter({
-    routes,
-    mode: 'history'
+  routes,
+  mode: 'history'
 });
 
 router.beforeEach((to, from, next) => {
-    const requiresAuth = to.matched.some(record => !record.meta.isPublic);
-    if (requiresAuth) {
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                next()
-            } else {
-                next({
-                    path: '/signin',
-                    query: {redirect: to.fullPath}
-                })
-            }
-        })
-    } else {
+  const requiresAuth = to.matched.some(record => !record.meta.isPublic);
+  if (requiresAuth) {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
         next()
-    }
+      } else {
+        next({
+          path: '/signin',
+          query: {redirect: to.fullPath}
+        })
+      }
+    })
+  } else {
+    next()
+  }
 });
 
 export default router;
