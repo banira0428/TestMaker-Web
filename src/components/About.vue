91e497e838e6cf486d7cec202690ed392eed1cdb
keita <banira0428@gmail.com>
2019-10-14 17:01:36 +0900

    ヘッダー　開閉部　修正 画像リセット

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="content-about">
-        <h2>このサイトについて</h2>
-        <p>このサイトは，スマートフォンアプリ「暗記メーカー」の問題作成をPCで効率的に行うためのサイトです。現在は問題作成機能しか実装していないので、その他の機能は要望に応じて順次追加して行こうと思います。</p>
-        <h2>アプリとの連携</h2>
-        <p>アプリ内の「みんなの問題集」→「マイページ」より、このサイトで作成した問題集をダウンロードすることができます。（ログインするアカウントを一致させてください）</p>
-        <h2>問い合わせ</h2>
-        <p>不具合の報告，追加機能の要望等は<a href="https://twitter.com/keita_developer" target="_blank">開発者</a>にご連絡ください</p>
-    </div>
+  <div class="content-about">
+    <h2>このサイトについて</h2>
+    <p>このサイトは，スマートフォンアプリ「暗記メーカー」(<a
+      href="https://play.google.com/store/apps/details?id=jp.gr.java_conf.foobar.testmaker.service&hl=ja" target="_blank">Android版</a>, <a
+      href="https://apps.apple.com/jp/app/テスト勉強用アプリ-暗記メーカー/id1201200202" target="_blank">iOS版</a>)
+      の問題作成をPCで効率的に行うためのサイトです。現在は問題作成機能しか実装していないので、その他の機能は要望に応じて順次追加して行こうと思います。</p>
+    <h2>アプリとの連携</h2>
+    <p>アプリ内の「みんなの問題集」→「マイページ」より、このサイトで作成した問題集をダウンロードすることができます。（ログインするアカウントを一致させてください）</p>
+    <h2>問い合わせ</h2>
+    <p>不具合の報告，追加機能の要望等は<a href="https://twitter.com/keita_developer" target="_blank">開発者</a>にご連絡ください</p>
+  </div>
 </template>
 
 <script>
