51870f2bbf5e4c709001188d676caac1c7abeffb
keita <banira0428@gmail.com>
2019-10-25 01:20:23 +0900

    アプリ紹介ページ　修正

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,15 +5,15 @@
     </div>
     <div class="text-center mt-sm-3">
       <h1 class="h2 pl-5 pr-5">テスト勉強用アプリ「暗記メーカー」</h1>
-      <div class="pb-3">
+      <div class="pb-5">
         <a
-          class="mr-2"
+          class="mr-2 ml-2"
           href="https://play.google.com/store/apps/details?id=jp.gr.java_conf.foobar.testmaker.service&hl=ja&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img
           src="../assets/googleplay.png"
           alt="Google Play で手に入れよう"
         ></a>
         <a
-          class="ml-2"
+          class="ml-2 mr-2"
           href="https://apps.apple.com/jp/app/%E3%83%86%E3%82%B9%E3%83%88%E5%8B%89%E5%BC%B7%E7%94%A8%E3%82%A2%E3%83%97%E3%83%AA-%E6%9A%97%E8%A8%98%E3%83%A1%E3%83%BC%E3%82%AB%E3%83%BC/id1201200202?mt=8"><img
           src="../assets/appstore.png"
           alt="App Store"
